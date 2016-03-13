b942ba77b06560cf3201c0a0a3da875393a733b1
丽萍 刘 <lipingliu@lipingdeMacBook-Pro.local>
2016-03-13 18:08:07 -0500

    fxu11

---
+++ code.c
@@ -0,0 +1,10 @@
+
+
+Class my_TCPIP {
+	public: 
+	int server();
+	int client();
+	private:
+} 
+
+//this is the basic c++ header file for my project, which would be filled with  other stuffs later.
